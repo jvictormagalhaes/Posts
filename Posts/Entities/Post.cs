3e7a56034f951e6130ff9c6165eca6d0fd3d9561
Victor Magalhães <vitormagaldino@gmail.com>
2023-01-30 21:37:18 -0300

    Criado programa para posts

---
+++ Posts/Entities/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using Posts.Entities;
+
+namespace Posts.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Comment> comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            comments.Add(comment);
+        }
+
+        public void RemotoComment(Comment comment)
+        {
+            comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder bs = new StringBuilder();
+            bs.AppendLine(Title);
+            bs.Append(Likes + " Likes - ");
+            bs.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            bs.AppendLine(Content);
+            bs.AppendLine("Comments: ");
+            foreach(Comment c in comments)
+            {
+                bs.AppendLine(c.Text);
+            }
+
+            return bs.ToString();
+        }
+    }
+}
---
+++ Posts/Program.cs
@@ -0,0 +1,27 @@
+﻿using Posts.Entities;
+using System;
+
+namespace Posts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice day!");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Comment c3 = new Comment("Good night !");
+            Comment c4 = new Comment("May the Force be with you!");
+
+            Post p1 = new Post(DateTime.Parse("21/06/2018 21:22:52"), "Traveling to New Zealend", "I'm going to visit this wonderful country", 12);
+            Post p2 = new Post(DateTime.Parse("28/07/2018 21:25:53"), "Good night guys", "See you tomorrow", 5);
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
